fb89caf9fe1c825d2c1d4848d1361fde606f68f1
Selva Ganesh M <selvadev2k@gmail.com>
2023-07-16 20:21:13 +0530

    created root context

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useRootContext } from '../hooks/useRootContext'
 
 type Props = Record<string, never>
 
 const Banner = (props: Props) => {
+    const { value, dispatch } = useRootContext();
     return (
-        <div>Banner</div>
+        <div className='h-[100vh] w-full bg-red-300'>
+            <div className='h-full w-full'>
+                <img src={value.banner} alt="" className='object-cover h-full' />
+            </div>
+        </div>
     )
 }
 
--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -7,11 +7,11 @@ const PageNotFound = (props: Props) => {
     const navigate = useNavigate();
     return (
         <div className="w-full h-[100vh] flex flex-col items-center justify-center gap-10">
-            <div className="w-[600px] h-[400px]">
+            <div className="w-[300px] h-[300px] sm:w-[500px]  md:w-[600px] md:h-[400px]">
                 <img src={pageNotfound} alt={'page not found - image'} className="w-full h-full object-contain" />
             </div>
             <div className="flex flex-col justify-center items-center gap-6 text-3xl">
-                <div className="text-center">You seem lost. Wanna play a Game!</div>
+                <div className="text-center">You seem lost.</div>
                 <button className="animate-bounce bg-blue-600 px-4 py-2 text-white rounded-xl" onClick={() => navigate("/")}>
                     Wanna Play a game!
                 </button>
