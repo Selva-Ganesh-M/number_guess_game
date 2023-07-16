a91a90f5fb57187a3df3abc5d635c1e58c902a3f
Selva Ganesh M <selvadev2k@gmail.com>
2023-07-16 15:16:59 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+type Props = Record<string, never>
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
